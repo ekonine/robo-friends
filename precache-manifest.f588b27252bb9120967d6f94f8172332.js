32636a8ca26e779b0a7a22f5954e6df9faa9622c
ekonine <xeon.slyde@gmail.com>
2020-01-20 21:35:04 -0600

    Updates

---
+++ precache-manifest.f588b27252bb9120967d6f94f8172332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b6e6c3316d747c869cda8c0ab88dd7",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "d29c30b08c08f0e2831d",
+    "url": "/robo-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "804c1a08d1ede1a4e770",
+    "url": "/robo-friends/static/css/main.70401bb2.chunk.css"
+  },
+  {
+    "revision": "d29c30b08c08f0e2831d",
+    "url": "/robo-friends/static/js/2.5f473242.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/robo-friends/static/js/2.5f473242.chunk.js.LICENSE"
+  },
+  {
+    "revision": "804c1a08d1ede1a4e770",
+    "url": "/robo-friends/static/js/main.b255ff78.chunk.js"
+  },
+  {
+    "revision": "6ba8ca054a370752743d",
+    "url": "/robo-friends/static/js/runtime-main.10a22b48.js"
+  }
+]);
