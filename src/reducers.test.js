e89d9ccebc464b3e0e5b63068c6816ee7756515c
ekonine <xeon.slyde@gmail.com>
2020-01-27 00:50:49 -0600

    add tests

---
+++ src/reducers.test.js
@@ -0,0 +1,88 @@
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_FAILED
+} from './constants.js';
+
+import * as actions from './actions.js';
+import * as reducers from './reducers.js';
+
+describe('searchRobots', () => {
+  const initialStateSearch = {
+    searchField: ''
+  }
+
+  it('should return the initial state', () => {
+    expect(reducers.searchRobots(undefined, {})).toEqual({ searchField: '' });
+  })
+
+  it('should handle CHANGE_SEARCH_FIELD', () => {
+    expect(
+      reducers
+      .searchRobots(
+        initialStateSearch, {
+          type: CHANGE_SEARCH_FIELD, //action
+          payload: 'abc'
+        }))
+      .toEqual({
+        searchField: 'abc'
+      });
+  })
+
+})
+
+describe('requestRobots', () => {
+  const initialStateRobots = {
+    isPending: false,
+    robots: [],
+    error: ''
+  }
+
+  it('should return the initial state', () => {
+    expect(reducers.requestRobots(undefined, {})).toEqual(initialStateRobots);
+  })
+
+
+  it('should handle REQUEST_ROBOTS_PENDING', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_PENDING
+    })).toEqual( {
+      robots: [],
+      isPending: true,
+      error: ''
+
+    });
+  })
+
+  it('should handle REQUEST_ROBOTS_SUCCESS', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_SUCCESS,
+      payload: [{
+        id: '123',
+        name: 'test',
+        email: 'test@gmail.com'
+      }]
+    })).toEqual( {
+      robots: [{
+        id: '123',
+        name: 'test',
+        email: 'test@gmail.com'
+      }],
+      isPending: false,
+      error: ''
+
+    });
+  })
+
+  it('should handle REQUEST_ROBOTS_FAILED', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_FAILED,
+      payload: 'failed'
+    })).toEqual({
+      isPending: false,
+      error: 'failed',
+      robots: []
+    });
+  })
+})
